e1c351df4ef2c8fe2458cff6f12e175b6bc2397a
Mark Baas <mark.baas@gmail.com>
2015-07-17 10:02:57 -0400

    update requirements

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='iresolve',
-      version='0.1.1',
+      version='0.1.3',
       description='Python Import Resolver',
       author='Mark Baas',
       author_email='mark.baas123@gmail.com',
       url='https://github.com/markbaas/python-iresolve',
       scripts=['iresolve'],
-      install_requires = ['pyflakes==0.8.1']
+      install_requires = ['pyflakes>=0.8.1']
      )
